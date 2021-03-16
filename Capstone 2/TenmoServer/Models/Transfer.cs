4c7260ba90e8ce04114551b52a37d619b9723c4b
Kevin Rasmussen <kevinlxndr@hotmail.com>
2021-03-16 19:03:28 -0400

    Add files via upload

---
+++ Capstone 2/TenmoServer/Models/Transfer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TenmoServer.Models
+{
+    public class Transfer
+    {
+        public int FromUserId { get; set; }
+        public int ToUserId { get; set; }
+        public decimal AmountTransfered { get; set; }
+        public int TransferId { get; set; }
+        public string ToUserName { get; set; }
+        public string FromUserName { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+
+
+
+    }
+}
